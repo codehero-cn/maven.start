b0504405f0002dd046d4c1f5b34ba73ed245fd6b
eleven <eleven@ling>
2015-08-21 00:43:55 +0800

    Signed-off-by: eleven <eleven@ling>

---
+++ src/main/java/cn/codehero/maven/main.java
@@ -0,0 +1,12 @@
+package cn.codehero.maven;
+/**
+ * 用来测试的main.java文件
+ * @author eleven
+ *
+ */
+public class main{
+	public static void main(String[] args){
+		System.out.println("this is maven test");
+	}
+	
+}
